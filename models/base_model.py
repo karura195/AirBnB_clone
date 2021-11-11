e5c0235d4ea2c0b938f4c67f1fc88fbc59ea8eab
unknown <jrichard1909@gmail.com>
2021-11-10 20:18:15 -0500

    Folders models and tests created. Base_models created

---
+++ models/base_model.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+from datetime import datetime
+from uuid import uuid4
+"""
+Module BaseModel
+"""
+
+class BaseModel():
+    """
+    Class BaseModel that defines all common attributes/methods
+    for other classes.
+    """
+
+    def __init__(self, *args):
+        """
+        This method allow the class to initialize the attributes id,
+        created_at and updated_at.
+        """
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """
+        This method returns the string representation as
+        [<class name>] (<self.id>) <self.__dict__>
+        """
+        return ('[{}] ({}) {}'.
+                format(self.__class__.__name__, self.id, self.__dict__))
+
+    def save(self):
+        """
+        Updates the public instance attribute updated_at
+        with the current datetime
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary containing all keys/values 
+        of __dict__ of the instance
+        """
+        dic = {}
+        dic = self.__dict__
+        dic['__class__'] = self.__class__.__name__
+        dic['created_at'] = self.created_at.isoformat()
+        dic['updated_at'] = self.updated_at.isoformat()
+        return dic
