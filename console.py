0941a52e7c6162eb536bc2c32afc8659c8c7f5cd
karura195 <karla.diaz@pucp.pe>
2021-11-14 16:58:11 -0800

    Console

--- console.py
+++ console.py
@@ -5,6 +5,13 @@ Program console.py
 import cmd
 from models.base_model import BaseModel
 from models import storage
+from datetime import datetime
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.user import User
 
 
 class HBNBCommand(cmd.Cmd):
